74b1a9248531618cae4a379e04d9feeb68546c6c
Görkem Taşçı <gorkem_tasci@hotmail.com>
2020-06-14 15:20:46 +0300

    Implementing swagger

--- src/main/java/com/tascigorkem/restaurantservice/RestaurantServiceApplication.java
+++ src/main/java/com/tascigorkem/restaurantservice/RestaurantServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
 @SpringBootApplication
 public class RestaurantServiceApplication {
 
